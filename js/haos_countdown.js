9ee5c4166b421c45b482acf5dbf337e1892b489c
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-04-20 09:00:37 +0000

    Add second countdown.

--- js/haos_countdown.js
+++ js/haos_countdown.js
@@ -57,12 +57,14 @@ function CountDownTimer(date_and_time_of_deadline, id) {
         // Finally, get to writing it...
         document.getElementById(id).innerHTML += '<b>CAPSTONE TIME!!</b></br> In : ';
 
+        document.getElementById(id).innerHTML += '<p>';
         document.getElementById(id).innerHTML += '<b>' + days + '</b> days, ';
         document.getElementById(id).innerHTML += '<b>' + hours + '</b> hours, ';
         document.getElementById(id).innerHTML += '<b>' + minutes + '</b> minutes, ';
         document.getElementById(id).innerHTML += '<b>' + seconds + '</b> secs; ';
         document.getElementById(id).innerHTML += '</br> <b>I PRESENT MY CAPSTONE!!</b>';
         document.getElementById(id).innerHTML += '</br>';
+        document.getElementById(id).innerHTML += '</p>';
     }
 
     // Update every second.
